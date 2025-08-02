35724f53024f9901b9d60e9dfde9c2e1c95f304d
Alejandro Propst <alexpropstg@gmail.com>
2025-08-02 19:26:45 +0200

    feat: add Logs, Tools, and Philosophy components with corresponding data

---
+++ src/data/logs.ts
@@ -0,0 +1,14 @@
+export const logs = [
+  {
+    date: "2025-07-28",
+    message: "updated this page and have started preparing to migrate it to Astro"
+  },
+  {
+    date: "2025-07-16",
+    message: "went back to Ubuntu for compatibility with some government programs"
+  },
+  {
+    date: "2025-07-02",
+    message: "fixed my mailserver"
+  },
+];
---
+++ src/data/tools.ts
@@ -0,0 +1,8 @@
+export const tools = [
+  { name: "HTML / CSS", icon: "/src/assets/img/logo/html-logo.png" },
+  { name: "JavaScript", icon: "/src/assets/img/logo/javascript-logo.png" },
+  { name: "Postfix & Dovecot", icon: "/src/assets/img/logo/postfix.png" },
+  { name: "Nginx / Apache", icon: "/src/assets/img/logo/apache-logo.png" },
+  { name: "Docker", icon: "/src/assets/img/logo/docker-logo.png" },
+  { name: "GNU/Linux bash", icon: "/src/assets/img/logo/Tux-logo.png" },
+];
