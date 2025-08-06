215dc60e6747286f54425ce9e435ae9d777703ec
Alejandro Propst <alexpropstg@gmail.com>
2025-08-06 21:32:20 +0200

    updated output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import react from '@astrojs/react';
 import node from "@astrojs/node";
-import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    preact(),
-    react(),
-  ],
+  output: "server",
   vite: {
     plugins: [tailwindcss()],
   },
