0449886f0b7e270af4405a98cebda7c4d9fefd7b
Alejandro Propst <alexpropstg@gmail.com>
2025-08-07 12:30:10 +0200

    updated & expanded tools section

--- src/data/tools.ts
+++ src/data/tools.ts
@@ -1,15 +1,27 @@
 import htmlLogo from "../assets/img/logo/html-logo.png";
+import tailwindcssicon from "../assets/img/logo/tailwindcss-icon.png";
+import astroicon from "../assets/img/logo/astro-icon.png";
 import jsLogo from "../assets/img/logo/javascript-logo.png";
+import giticon from "../assets/img/logo/git-icon.png";
+import filezillaicon from "../assets/img/logo/FileZilla-icon.png";
 import postfixLogo from "../assets/img/logo/postfix.png";
+import nginxicon from "../assets/img/logo/nginx-icon.png";
 import apacheLogo from "../assets/img/logo/apache-logo.png";
+import dnsLogo from "../assets/img/logo/dns-icon.png";
 import dockerLogo from "../assets/img/logo/docker-logo.png";
 import tuxLogo from "../assets/img/logo/Tux-logo.png";
 
 export const tools = [
-  { name: "HTML / CSS", icon: htmlLogo },
+  { name: "HTML", icon: htmlLogo },
+  { name: "TailwindCSS", icon: tailwindcssicon },
+  { name: "Astro", icon: astroicon },
   { name: "JavaScript", icon: jsLogo },
+  { name: "Git", icon: giticon},
+  { name: "Filezilla", icon: filezillaicon},
   { name: "Postfix & Dovecot", icon: postfixLogo },
-  { name: "Nginx / Apache", icon: apacheLogo },
+  { name: "Nginx", icon: nginxicon },
+  { name: "Apache", icon: apacheLogo },
+  { name: "DNS records", icon: dnsLogo},
   { name: "Docker", icon: dockerLogo },
   { name: "GNU/Linux bash", icon: tuxLogo },
 ];
