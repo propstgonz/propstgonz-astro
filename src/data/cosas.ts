a9b2fdd452c821e1ea8ad2480a5fbc403942d5a0
propstgonz <propstgonz@baronette.es>
2025-11-24 21:23:08 +0100

    Mapped hub things (Unfinished)

--- src/data/cosas.ts
+++ src/data/cosas.ts
@@ -11,44 +11,35 @@ import type { Cosa } from "../types/cosas";
 */
 
 export const cosas: Cosa[] = [
-    {
+  {
     img: "/img/FAQ.svg",
     title: "FAQ",
-    description: "Preguntas y respuestas sobre la página",
+    description: "Questions and answers about me",
     link: "/faq",
   },
   {
-    img: "https://fasealterna.es/_astro/background-1.D_sJXt5u.webp",
-    title: "Fase Alterna",
-    description: "El grupo del puto Mezzenas (Págame basura)",
-    link: "https://fasealterna.es/",
+    img: "",
+    title: "Baronette web",
+    description: "The domain main page (In progress",
+    link: "https://baronette.es",
   },
-
-/*
-## Aquí irá la página graciosa que le voy a dedicar a Javier Tebas
   {
     img: "",
-    title: "",
-    description: "",
-    link: "",
-  },
-*/
-  {
-    img: "/img/pokedle.png",
-    title: "PokéDle",
-    description: "Un juego de adivinar pokémons para gente de 300 kg by Patinni",
-    link: "http://patinni.duckdns.org/",
+    title: "GitHub",
+    description: "My GitHub profile containing all my development projects",
+    link: "https://github.com/propstgonz",
   },
   {
-    img: "/img/trollface.png",
-    title: "Mi primera web",
-    description: "Tal cual, mi primera web",
+    img: "",
+    title: "Projects",
+    description: "Where I display all my deployed services",
+    link: "/projects",
+  },
+  {
+    img: "",
+    title: "My first page",
+    description: "My first HTML page, simple, but iconic.",
     link: "http://localhost:80",
   },
-  {
-    img: "/img/Baronette.png",
-    title: "baronette.es (Primera versión)",
-    description: "La primera versión de Baronette.es",
-    link: "https://github.com/propstgonz/baronette-webapp-old",
-  },
+
 ];
