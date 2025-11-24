e5dd57be0ec660022559e63fece019962d7fd09e
propstgonz <propstgonz@baronette.es>
2025-11-24 23:10:30 +0100

    Add trollface image and update links and descriptions in Hub component

--- src/data/cosas.ts
+++ src/data/cosas.ts
@@ -12,34 +12,39 @@ import type { Cosa } from "../types/cosas";
 
 export const cosas: Cosa[] = [
   {
-    img: "/img/FAQ.svg",
+    img: "/favicon.svg",
     title: "FAQ",
     description: "Questions and answers about me",
     link: "/faq",
   },
   {
-    img: "",
+    img: "https://baronette.es/_image?href=%2F_astro%2FBaronette-minimalista-cuadrado.Bjgv9ofB.png&w=1024&h=1024&f=webp",
     title: "Baronette web",
-    description: "The domain main page (In progress",
+    description: "The domain main page (In progress)",
     link: "https://baronette.es",
   },
   {
-    img: "",
+    img: "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
     title: "GitHub",
     description: "My GitHub profile containing all my development projects",
     link: "https://github.com/propstgonz",
   },
   {
-    img: "",
+    img: "/logbook.png",
     title: "Projects",
     description: "Where I display all my deployed services",
     link: "/projects",
   },
   {
-    img: "",
+    img: "/trollface.png",
     title: "My first page",
     description: "My first HTML page, simple, but iconic.",
     link: "http://localhost:80",
   },
-
+  {
+    img: "https://pbs.twimg.com/profile_images/1866395025740869632/bjM0BTeb_400x400.jpg",
+    title: "SHITTY RANDOM SITE",
+    description: "Random site",
+    link: "https://darkweb-websites.com/",
+  },
 ];
