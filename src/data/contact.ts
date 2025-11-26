68335e3775a0674904c48b3d750e1c1738681083
propstgonz <propstgonz@baronette.es>
2025-11-26 22:23:47 +0100

    Add Instagram SVG icon and update contact component
    - Added a new SVG icon for Instagram to the project.
    - Updated the Contact component to display social media icons instead of text labels.
    - Modified the contact data structure to include icon paths for GitHub, Discord, Email, and Instagram.

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,22 +1,26 @@
 export const contact = [
   {
-    label: 'GitHub',
-    href: 'https://github.com/propstgonz',
+    label: "GitHub",
+    href: "https://github.com/propstgonz",
     external: true,
+    icon: "/icons/svg/github.svg",
   },
   {
-    label: 'Discord',
-    href: 'https://discord.com/users/1000141861333258393',
+    label: "Discord",
+    href: "https://discord.com/users/1000141861333258393",
     external: true,
+    icon: "/icons/svg/discord.svg",
   },
   {
-    label: 'Email',
-    href: 'mailto:propstgonz@baronette.es',
+    label: "Email",
+    href: "mailto:propstgonz@baronette.es",
     external: false,
+    icon: "/icons/svg/email.svg",
   },
-    {
-    label: 'Instagram',
-    href: 'https://www.instagram.com/propstgonz/',
+  {
+    label: "Instagram",
+    href: "https://www.instagram.com/propstgonz/",
     external: true,
+    icon: "/icons/svg/instagram.svg",
   },
 ];
